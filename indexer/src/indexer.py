8bb8592bd23b5fc1ca23d42fb3d642282e3d6436
Sebastian Muthwill <sebastian@sebserver.de>
2023-11-20 21:49:38 +0100

    first running version

---
+++ indexer/src/indexer.py
@@ -0,0 +1,103 @@
+import logging
+import json
+from opensearchpy import OpenSearch
+from time import sleep
+from confluent_kafka import Consumer
+from confluent_kafka.admin import AdminClient
+
+logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
+logging.info("indexer container started")
+logging.info("Waiting for Kafka broker and Opensearch to be ready. Sleep for 20s.")
+
+kafka_conf = {'bootstrap.servers': 'kafka:9092', 'group.id': 'indexer-1'}
+
+host = 'opensearch'
+port = 9200
+auth = ('admin', 'admin') # For testing only. Don't store credentials in code.
+
+def get_kafka_broker():
+    consumer_instance = Consumer(kafka_conf)
+    logging.debug("Trying if Kafka broker is running.")
+    # Check if kafka broker is running using confuent_kafka.admin
+    kafka_broker = {'bootstrap.servers': 'kafka:9092'}
+    admin_client = AdminClient(kafka_broker)
+    topics = admin_client.list_topics().topics
+
+    if not topics: 
+        logging.debug("No respose from Kafka broker, so it is not running yet. Will wait and try again.")
+        return None
+    logging.info("Kafka broker is running.")
+    return consumer_instance
+
+
+consumer = None
+while not consumer:
+    sleep(2)
+    consumer = get_kafka_broker()
+    
+consumer.subscribe(['blog.new_blogpost'])
+
+def get_opensearch_client():
+    try:
+        logging.info("Trying if Opensearch is running.")
+        client = OpenSearch(
+            hosts = [{'host': host, 'port': port}],
+            http_auth = auth,
+            use_ssl = True,            
+            verify_certs = False,       # DONT USE IN PRODUCTION
+            ssl_assert_hostname = False,
+            ssl_show_warn = False
+        )
+        info = client.info()
+        logging.info(f"Welcome to {info['version']['distribution']} {info['version']['number']}!")
+    except Exception as e:
+        logging.debug("No respose from Opensearch, so it is not running yet. Will wait and try again.")
+        sleep(5)
+        return None
+    return client
+
+client = None
+while not client:
+    client = get_opensearch_client()
+
+
+try:
+    index_name = 'blog-index'
+    index_body = {
+    'settings': {
+        'index': {
+        'number_of_shards': 4
+        }
+    }
+    }
+
+    response = client.indices.create(
+    index_name, 
+    body=index_body
+    )
+
+    logging.debug(response)
+except Exception as e:
+    pass
+
+while True:
+    msg = consumer.poll(1.0)
+
+    if msg is None:
+        continue
+
+    if msg.error():
+        logging.error("Consumer error: {}".format(msg.error()))
+        continue
+
+    logging.info('Received message: {}'.format(msg.value().decode('utf-8')))
+    blog = json.loads(msg.value().decode('utf-8'))
+    logging.info('Blog title: {}'.format(blog['title']))
+
+    response = client.index(
+    index = index_name,
+    body = blog,
+    id = blog['id'],
+    refresh = True
+    )
+    logging.debug(response)
---
+++ producer/src/producer.py
@@ -0,0 +1,51 @@
+from faker import Faker
+import json
+from uuid import uuid4
+from confluent_kafka import Producer
+from confluent_kafka.admin import AdminClient
+import socket
+from time import sleep
+import logging
+
+# Container logs are not printed to stdout by default.
+# Therefor we need to configure logging to print to stdout.
+logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
+
+logging.info("producer container started")
+kafka_conf = {'bootstrap.servers': 'kafka:9092',
+        'client.id': "producer-1"}
+
+def get_producer():
+    producer_instance = Producer(kafka_conf)
+    logging.debug("Trying if Kafka broker is running.")
+    # Check if kafka broker is running using confuent_kafka.admin
+    kafka_broker = {'bootstrap.servers': 'kafka:9092'}
+    admin_client = AdminClient(kafka_broker)
+    topics = admin_client.list_topics().topics
+
+    if not topics: 
+        logging.debug("No respose from Kafka broker, so it is not running yet. Will wait and try again.")
+        return None
+    logging.info("Kafka broker is running.")
+    return producer_instance
+
+producer = None
+while not producer:
+    sleep(2)
+    producer = get_producer()
+
+fake = Faker()
+
+while True:
+    blog = {
+        "id": str(uuid4()),
+        "title": fake.sentence(),
+        "body": fake.text(800),
+        "author": fake.name(),
+        "created_at": str(fake.date_time_this_decade()),
+        "url": fake.url(),
+    }
+
+    producer.produce('blog.new_blogpost', json.dumps(blog))
+    logging.info(f"Blog {blog['id']} published successfully.")
+    sleep(2)
